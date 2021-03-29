2a855385476e955087bf427ead7dee6f8ac7aeb3
Henrique Lopes <henriquehendell@gmail.com>
2021-03-29 00:58:47 -0300

    feat: card-content

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,12 +4,33 @@ import {  } from 'react-native'
 import Header from '../components/Header';
 import Tabs from '../components/Tabs';
 
-import {Container} from './styles'
+import {MaterialIcons} from '@expo/vector-icons';
+
+import {Container, Content, Card, CardHeader, CardContent, CardFooter, Title, Description, Annotation } from './styles'
 
 export default function Home() {
     return (
         <Container>
             <Header />
+
+            <Content>
+                <Card>
+                    <CardHeader>
+                        <MaterialIcons name='attach-money' size={28} color='#666' /> 
+                        <MaterialIcons name='visibility-off' size={28} color='#666' /> 
+                    </CardHeader>
+
+                    <CardContent>
+                        <Title>Saldo Disponível</Title>
+                        <Description>R$ 197.611,65</Description>
+                    </CardContent>
+
+                    <CardFooter>
+                        <Annotation>Transferência de R$ 100,00 recebida de Henrique Hendel Santos Lopes hoje às 17:00h</Annotation>
+                    </CardFooter>
+                </Card>
+            </Content>
+
             <Tabs />
         </Container>
     )
